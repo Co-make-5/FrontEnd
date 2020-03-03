1d8f2cb603fd28b966fed5643c78c01da625ba96
KyleCole01 <kyle@misfitselectronics.com>
2020-03-03 14:41:49 -0600

    add NavBar framework

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = (props) => {
 
     return (
-        <>
-
-        </>
-    )
+      <div className="nav-container">
+        <NavLink to="/open" activeClassName="selected" className="nav">
+          Issues
+        </NavLink>
+        <NavLink to="/closed" activeClassName="selected" className="nav">
+          Closed Issues
+        </NavLink>
+        <NavLink to="/new" activeClassName="selected" className="nav">
+          Create Issue
+        </NavLink>
+      </div>
+    );
 }
 
 export default NavBar
