43d0a55d0b1ce11fa5d23462aa966f8512861d2a
KyleCole01 <kyle@misfitselectronics.com>
2020-03-03 17:26:18 -0600

    revert NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
 
-const NavBar = (props) => {
+const NavBar = props => {
+  return <></>;
+};
 
-    return (
-      <div className="nav-container">
-        <NavLink to="/open" activeClassName="selected" className="nav">
-          Issues
-        </NavLink>
-        <NavLink to="/closed" activeClassName="selected" className="nav">
-          Closed Issues
-        </NavLink>
-        <NavLink to="/new" activeClassName="selected" className="nav">
-          Create Issue
-        </NavLink>
-      </div>
-    );
-}
-
-export default NavBar
+export default NavBar;
