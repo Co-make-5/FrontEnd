0b9cedec702d823f6ef31c6acc83fb80bc9bc70c
MaxiCB <aaroncb@aaroncb.com>
2020-03-04 12:06:16 -0600

    FIX UserOutline, LockOutline imports
    - When merging into master the two imports were overwritten

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,7 @@ import { Link } from "react-router-dom";
 // Ant Design
 import '@ant-design/compatible/assets/index.css';
 import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 const Login = (props) => {
 
