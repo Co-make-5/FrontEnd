8326b285e146f5fea3d69fc3346f129520ff1e96
Elysia Gabe <elysiagabe@gmail.com>
2020-03-05 12:05:35 -0800

    Adds toggle switch and search input to Explore.js

--- src/components/Pages/Explore.js
+++ src/components/Pages/Explore.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import PieChart from '../Accents/PieChart';
+import { Row, Input, Switch } from 'antd';
+
+const { Search } = Input;
 
 const Explore = (props) => {
+    //onChange placeholder
+    let onChange = (checked) => {
+        console.log(`switch to ${checked}`)
+    }
 
     return (
         <div style={{height: '80vh'}}>
-            
+            <Row justify="space-around" align="middle">
+                <Switch 
+                    defaultChecked
+                    onChange={onChange}
+                />
+                <Search 
+                    placeholder="Search..."
+                    onSearch={value => console.log(value)}
+                    style={{ width: 300 }}
+                    enterButton
+                />
+            </Row>
             <PieChart/>
         </div>
     )
